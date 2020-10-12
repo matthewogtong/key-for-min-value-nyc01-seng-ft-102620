fe082aed515ef8980ab1cf0513e303ad85550dbc
Matthew Ogtong <ogtongm@gmail.com>
2020-10-12 23:23:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_value = nil
 
   name_hash.each do |name, value|
-    if name && value < min_value
+    if value < min_value
       min_value = value
     end
   end
