c2024ecd04da28d837a48840c032b9282f4c2dff
Matthew Ogtong <ogtongm@gmail.com>
2020-10-13 00:20:05 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,19 @@
 
 def key_for_min_value(name_hash)
 
+  min_value = 999999999999999999999
+
+  name_hash.each do |name, value|
+
+
+    if !name
+      min_value = nil
+    elsif value < min_value
+      min_value = value
+    end
+  end
+
+
+  name_hash.key(min_value)
+
 end
